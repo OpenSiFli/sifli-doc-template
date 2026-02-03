7c0f7a88d6f5dba3ea588a2d51f3aa7683a5cbad
Xiaoming Xi <xiaomingxi@sifli.com>
2026-02-03 14:09:43 +0800

    Add default configuration

--- source/en/conf.py
+++ source/en/conf.py
@@ -16,3 +16,17 @@ project = 'foo'
 # del html_context['versions']
 # uncomment this this line if you want to hide language listbox
 # del html_context['languages']
+
+# chip = 'SF32LB52x'
+# doc_id = 'RPT5202'
+# doc_name = 'Low Power Consumption Scenario Testing Report'
+# doc_dir = 'rpt5202_sf32lb52x-low-power-measurement-report'
+
+# html_context.update({
+#     'chip': chip,
+#     'doc_id': doc_id,
+#     'doc_name': doc_name,
+#     })
+
+# pdf_url_enabled = True
+
